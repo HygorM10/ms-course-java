20968a218fce5b12ce8325fbaa126274565be000
Hygor Martins <53188282+HygorM10@users.noreply.github.com>
2020-11-24 08:41:30 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/hygor/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hygor.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
