7735a9e5a551c0cf590b4a214f8f18ea153de9b0
Hygor Martins <53188282+HygorM10@users.noreply.github.com>
2020-12-11 22:25:47 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/hygor/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/hygor/hroauth/entities/User.java
@@ -1,7 +1,15 @@
 package com.hygor.hroauth.entities;
 
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,12 +17,40 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class User {
+public class User implements UserDetails, Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
 	private String email;
 	private String password;
+	
 	private Set<Role> roles = new HashSet<>();
 	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+					.collect(Collectors.toList());
+	}
+	@Override
+	public String getUsername() {
+		return email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
